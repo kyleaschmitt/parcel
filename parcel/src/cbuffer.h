08f61b9d7c7c427b280704e3bb3965f4bd941451
Joshua Miller <jshuasmiller@gmail.com>
2015-04-08 23:04:56 +0000

    update cbuffer.h

--- parcel/src/cbuffer.h
+++ parcel/src/cbuffer.h
@@ -1,33 +1,9 @@
-/******************************************************************************
- *
- * FILE    : cbuffer.h
- * AUTHOR  : Joshua Miller
- *           jshuasmiller@gmail.com              _
- * PROJECT : parcel                             | |
- *                      _ __   __ _ _ __ ___ ___| |
- *                     | '_ \ / _` | '__/ __/ _ \ |
- *                     | |_) | (_| | | | (_|  __/ |
- *                     | .__/ \__,_|_|  \___\___|_|
- *                     | |
- *                     |_|
- *
- * DESCRIPTION : This file contains CircularBuffer class
- *
- * LICENSE : Licensed under the Apache License, Version 2.0 (the
- *           "License"); you may not use this file except in
- *           compliance with the License.  You may obtain a copy of
- *           the License at
- *
- *               http://www.apache.org/licenses/LICENSE-2.0
- *
- *           Unless required by applicable law or agreed to in
- *           writing, software distributed under the License is
- *           distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- *           CONDITIONS OF ANY KIND, either express or implied.  See
- *           the License for the specific language governing
- *           permissions and limitations under the License.)
- *
- ******************************************************************************/
+/*
+ * File taken from
+ * http://www.asawicki.info/news_1468_circular_buffer_of_raw_binary_data_in_c.html
+ * and modified to be thread safe for posix systems and to block on write.
+ */
+
 #ifndef __CBUFFER_H__
 #define __CBUFFER_H__
 
